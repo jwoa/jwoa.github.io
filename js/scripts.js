307c31bd2a5b64d089f369849dd93115801829ca
Jason <jason@seasonsix.co>
2018-10-03 01:02:17 -0400

    SICKO MODE Debugging

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,12 @@
 //### SICKO MODE ###//
 
 window.onload = function() {
-    
+    console.log("Booted.");
     function sickoMode() {
         var body = document.getElementById("body");
         var currentClass = body.className;
         body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
     }
-       
+    console.log("Ended.");
 };
+
