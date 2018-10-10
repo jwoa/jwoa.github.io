dc02aa7e078e24d0b7123cb3422e9c98f2049d1f
Jason <jason@seasonsix.co>
2018-10-10 11:13:47 -0400

    Dark Mode

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,22 @@
-// All the fancy shmancy javascript stuff.
-
 //### SICKO MODE ###//
 
-window.onload = function() {
-    console.log("Booted.");
-    function sickoMode() {
-        var body = document.getElementById("body");
-        var currentClass = body.className;
-        body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
-    }
-    console.log("Ended.");
-};
+document.getElementById('theme').addEventListener('click', function() {
+   let sickoModeOn = document.body.classList.toggle('dark-mode');
+   localStorage.setItem('dark-mode-on', sickoModeOn);
+});
 
+if (JSON.parse(localStorage.getItem('dark-mode-on'))) {
+    document.body.classList.add('dark-mode');
+}
+
+//    function sickoMode() {
+//        var body = document.getElementById("body");
+//        var currentClass = body.className;
+//        body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+//        
+//        localStorage.setItem('body', 'currentClass');
+//    }
+    
+//    document.getElementById('theme').addEventListener('click', function() {
+//   document.body.classList.toggle('dark-mode');
+//});
